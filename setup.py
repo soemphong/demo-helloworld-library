4445281b7ebc04fdb9f3d11b4517f8fd47d303c7
soemphong <soemphong.j@gmail.com>
2019-04-19 12:06:57 -0700

    Hello First Library

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name="helloworld-library",
+    name="Pong_helloworld-library",
     version='1.0',
     package_dir={'' : 'src'},
     packages=['HelloWorld'],
-    url='https://github.com/up1/demo-helloworld-library',
-    author='Somkiat',
-    author_email='somkiat.p@gmail.com',
+    url='https://github.com/soemphong/demo-helloworld-library',
+    author='Soemphong',
+    author_email='soemphong.j@gmail.com',
 )
